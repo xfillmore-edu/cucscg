453dcd6eedb5cf51da36cf4ce408b148bb52ec2e
xfillmore <xuedan.fillmore@colorado.edu>
2020-06-21 21:58:09 -0600

    modified lighting settings for cylinder. Lighting still not right.

--- hw3/cylinder.c
+++ hw3/cylinder.c
@@ -5,6 +5,14 @@ void cylinder(double xcen, double ycen, double zcen, double height, double radiu
     int dd    = 30; /* change in degrees ~ visual precision */
     int theta = 0;  /* current angle pos */
 
+    /* lighting effects */
+    float shiny[] = {30};
+    float whtsp[] = {1.0, 1.0, 1.0, 1.0};
+    glMaterialfv(GL_FRONT, GL_SHININESS, shiny);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, whtsp);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, whtsp);
+    glEnable(GL_RESCALE_NORMAL);
+
     /* save transformations */
     glPushMatrix();
 
@@ -25,7 +33,8 @@ void cylinder(double xcen, double ycen, double zcen, double height, double radiu
     /* cylinder cap */
     glBegin(GL_TRIANGLE_FAN);
     glNormal3f(0, 1, 0);
-    glColor3f(205.0/rgbmax, 227.0/rgbmax, 157.0/rgbmax);
+    // glColor3f(205.0/rgbmax, 227.0/rgbmax, 157.0/rgbmax);
+    glColor3f(1.0, 1.0, 1.0);
     glVertex3d(0, height, 0);
     for (theta = 0; theta <= 360; theta += dd)
     {
@@ -36,7 +45,8 @@ void cylinder(double xcen, double ycen, double zcen, double height, double radiu
     /* cylinder base */
     glBegin(GL_TRIANGLE_FAN);
     glNormal3f(0, -1, 0);
-    glColor3f(205.0/rgbmax, 227.0/rgbmax, 157.0/rgbmax);
+    // glColor3f(205.0/rgbmax, 227.0/rgbmax, 157.0/rgbmax);
+    glColor3f(1.0, 1.0, 1.0);
     glVertex3d(0, 0, 0);
     for (theta = 360; theta >= 0; theta -=dd)
     {
