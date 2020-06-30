da2ee4f9541fa52e3a3d61d6970ab60fe1bc3be5
xfillmore <xuedan.fillmore@colorado.edu>
2020-06-29 21:55:09 -0600

    Updated project function to handle selection pick matrix

--- proj/project.cpp
+++ proj/project.cpp
@@ -3,18 +3,22 @@
 
 #include "config.hpp"
 
-void project(double fov, double aspratio, double dimension)
+void project(double fov, double aspratio, double dimension, int mxpos, int mypos)
 {
-    /* manipulate projection matrix */
+    // manipulate projection matrix
     glMatrixMode(GL_PROJECTION);
 
-    /* remove previous transformations */
+    // remove previous transformations
     glLoadIdentity();
 
-    /* not inherited from reshape() */
+    // Picking/Selection modifiers
+    // http://unknownware.com/rtfm/graphics/glselection.html
+    GLint viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+    gluPickMatrix((GLdouble)mxpos, (GLdouble)(viewport[3]-mypos), 0.2, 0.2, viewport );
 
-    /* perspective transformation */
-    /* 0 - orthogonal */
+    // perspective transformation
+    // 0 - orthogonal
     if (fov)
     {
         gluPerspective(fov, aspratio, dimension/16, 16*dimension);
@@ -24,9 +28,9 @@ void project(double fov, double aspratio, double dimension)
         glOrtho(-aspratio * dimension, aspratio * dimension, -dimension, dimension, -dimension, dimension);
     }
 
-    /* back to model matrix */
+    // back to model matrix
     glMatrixMode(GL_MODELVIEW);
 
-    /* clear transformations */
+    // clear transformations
     glLoadIdentity();
 }
